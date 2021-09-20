7ee6192e572851b985a111495976947297a48393
lara1991 <32224979+lara1991@users.noreply.github.com>
2021-09-20 08:09:09 +0530

    Update train.py

--- keras_retinanet/bin/train.py
+++ keras_retinanet/bin/train.py
@@ -151,15 +151,17 @@ def create_callbacks(model, training_model, prediction_model, validation_generat
         makedirs(args.tensorboard_dir)
         update_freq = args.tensorboard_freq
         hist_freq = args.tensorboard_hist_freq
+        write_images_str = args.tb_images
+        write_imgs = False if (write_images_str=="false") else True
         if update_freq not in ['epoch', 'batch']:
             update_freq = int(update_freq)
         tensorboard_callback = keras.callbacks.TensorBoard(
             log_dir                = args.tensorboard_dir,
-            histogram_freq         = 1,
+            histogram_freq         = hist_freq,
             batch_size             = args.batch_size,
             write_graph            = True,
             write_grads            = False,
-            write_images           = True,
+            write_images           = write_imgs,
             update_freq            = update_freq,
             embeddings_freq        = 0,
             embeddings_layer_names = None,
@@ -439,7 +441,8 @@ def parse_args(args):
     parser.add_argument('--snapshot-path',    help='Path to store snapshots of models during training (defaults to \'./snapshots\')', default='./snapshots')
     parser.add_argument('--tensorboard-dir',  help='Log directory for Tensorboard output', default='')  # default='./logs') => https://github.com/tensorflow/tensorflow/pull/34870
     parser.add_argument('--tensorboard-freq', help='Update frequency for Tensorboard output. Values \'epoch\', \'batch\' or int', default='epoch')
-    parser.add_argument('--tensorboard-hist-freq', help='Save model layer histograms for Tensorboard output. Values \'epoch\', \'batch\' or int', default=0)
+    parser.add_argument('--tensorboard-hist-freq', help='Save model layer histograms for Tensorboard output. Values \'epoch\', \'batch\' or int', default=1)
+    parser.add_argument('--tb-images', help='Save images for Tensorboard output. Values \'epoch\', \'batch\' or int', default="false")
     parser.add_argument('--no-snapshots',     help='Disable saving snapshots.', dest='snapshots', action='store_false')
     parser.add_argument('--no-evaluation',    help='Disable per epoch evaluation.', dest='evaluation', action='store_false')
     parser.add_argument('--freeze-backbone',  help='Freeze training of backbone layers.', action='store_true')
